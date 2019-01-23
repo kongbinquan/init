b8b083d838a5d5bf4b81bf869d84ddfd58e47fe2
root <root@Node104_244_89_228.cluster.com>
2019-01-23 05:57:48 -0500

    添加初始化内容

--- main.sh
+++ main.sh
@@ -127,6 +127,62 @@ updateYUM() {
 	[ "$release" = "7" ] && yum -y groups install "Development Tools" "Server Platform Development"
 }
 
+
+#优化内核参数，并备份原有文件。
+setSYSCTL() {
+	cp /etc/sysctl.conf{,_$(date "+%Y%m%d_%H%M%S")_backup}
+	cat > /etc/sysctl.conf <<-EOF
+		fs.file-max=65535
+		net.ipv4.tcp_max_tw_buckets = 60000
+		net.ipv4.tcp_sack = 1
+		net.ipv4.tcp_window_scaling = 1
+		net.ipv4.tcp_rmem = 4096 87380 4194304
+		net.ipv4.tcp_wmem = 4096 16384 4194304
+		net.ipv4.tcp_max_syn_backlog = 65536
+		net.core.netdev_max_backlog = 32768
+		net.core.somaxconn = 32768
+		net.core.wmem_default = 8388608
+		net.core.rmem_default = 8388608
+		net.core.rmem_max = 16777216
+		net.core.wmem_max = 16777216
+		net.ipv4.tcp_timestamps = 0
+		net.ipv4.tcp_synack_retries = 2
+		net.ipv4.tcp_syn_retries = 2
+		#net.ipv4.tcp_tw_recycle = 1
+		#net.ipv4.tcp_tw_len = 1
+		net.ipv4.tcp_tw_reuse = 1
+		net.ipv4.tcp_mem = 94500000 915000000 927000000
+		net.ipv4.tcp_max_orphans = 3276800
+		net.ipv4.ip_local_port_range = 1024 65000
+
+		net.nf_conntrack_max = 6553500
+		net.netfilter.nf_conntrack_max = 6553500
+		net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60
+		net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 120
+		net.netfilter.nf_conntrack_tcp_timeout_time_wait = 120
+		net.netfilter.nf_conntrack_tcp_timeout_established = 3600
+
+		net.ipv4.conf.all.rp_filter = 2
+		net.ipv4.ip_forward = 1
+	EOF
+}
+changZHCN() {
+	if [ "$release" = "6" ]; then
+		yum groupinstall "Chinese Support" -y
+		cat > /etc/sysconfig/i18n <<-EOF
+			#LANG=C
+			#SYSFONT=latarcyrheb-sun16
+			LANG="zh_CN.UTF-8"
+			SYSFONT="latarcyrheb-sun16"
+			SUPPORTED="zh_CN.UTF-8:zh_CN:zh"
+		EOF
+	elif [ "$release" = "7" ]; then
+		localectl set-locale LANG=zh_CN.utf8
+		localectl set-keymap cn
+		localectl set-x11-keymap cn
+	fi
+}
+
 disable_ipv6
 add_yum_pulgins
 sync_time
@@ -134,7 +190,9 @@ ssh_config
 #install_zabbix
 setSELinux
 install_docker
+setSYSCTL && sysctl -p
 setPS1
+changZHCN
 updateYUM
 ss -tnl
 
